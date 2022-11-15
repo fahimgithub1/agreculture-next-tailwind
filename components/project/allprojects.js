f61ae948dba6a0f64d9b5bba4afa4d1e2d3d2c86
unknown <fahimjoy768@gmial.com>
2022-11-15 09:01:16 +0600

    pixed arror

--- components/project/allprojects.js
+++ components/project/allprojects.js
@@ -4,31 +4,37 @@ import PerProjectThemnel from "./perprojectthemnel";
 
 const ProjectThemnel = [
     {
+        id: 1,
         upertext : "HEALTHY",
         text : "ORGANIC SOLUTIONS",
         img : require("/public/projects/project-one-1.jpg")
     },
     {
+        id: 2,
         upertext : "FARMING",
         text : "HARVEST INNOVATIONS",
         img : require("/public/projects/project-one-2.jpg")
     },
     {
+        id: 3,
         upertext : "SOLUTION",
         text : "THE FARMING SEASON",
         img : require("/public/projects/project-one-3.jpg")
     },
     {
+        id: 4,
         upertext : "HEALTHY",
         text : "ORGANIC SOLUTIONS",
         img : require("/public/projects/project-one-4.jpg")
     },
     {
+        id: 5,
         upertext : "HARVEST",
         text : "ECOLOGICAL FARMING",
         img : require("/public/projects/project-one-5.jpg")
     },
     {
+        id: 6,
         upertext : "FARMING",
         text : "ECOLOGICAL FARMING",
         img : require("/public/projects/project-one-6.jpg")
@@ -43,6 +49,7 @@ export default function AllProjects(){
 
             {ProjectThemnel.map((item)=>(
                 <PerProjectThemnel 
+                                    key={item.id}
                                     img = {item.img}
                                     upertext={item.upertext}
                                     text = {item.text}
